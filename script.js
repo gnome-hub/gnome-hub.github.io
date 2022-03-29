0a5e377ce12b7c98e03cbaf4c13f9c7338871411
varora24 <varora@nd.edu>
2022-03-29 16:32:23 -0400

    fixed navbar bug for background

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
     $(window).scroll(function(){
         // sticky navbar on scroll script
         if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
+            $('.navbarcustom').addClass("sticky");
         }else{
-            $('.navbar').removeClass("sticky");
+            $('.navbarcustom').removeClass("sticky");
         }
         
         // scroll-up button show/hide script
@@ -22,14 +22,14 @@ $(document).ready(function(){
         $('html').css("scrollBehavior", "auto");
     });
 
-    $('.navbar .menu li a').click(function(){
+    $('.navbarcustom .menu li a').click(function(){
         // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
+        $('.navbarcustom .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
